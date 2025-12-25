6bdf4457937206ebb9e098f3c23c3ea03c960403
Evgeny Zherdev <zheny.zherdev@bk.ru>
2025-12-25 12:07:22 +0700

    fix

--- src/common/components/linear-progress/linear-progress.tsx
+++ src/common/components/linear-progress/linear-progress.tsx
@@ -6,7 +6,6 @@ type Props = {
 
 export const LinearProgress = ({ height = 4 }: Props) => {
   return (
-    //
     <div className={s.root} style={{ height }}>
       <div className={`${s.bar} ${s.indeterminate1}`} />
       <div className={`${s.bar} ${s.indeterminate2}`} />
