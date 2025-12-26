af0ee2f828b443e4e5bd2c9f76352b0ed888ced9
Evgeny Zherdev <zheny.zherdev@bk.ru>
2025-12-26 14:21:04 +0700

    fix

--- src/features/playlists/ui/playlists-page.tsx
+++ src/features/playlists/ui/playlists-page.tsx
@@ -1,55 +1,57 @@
-import { type ChangeEvent, useState } from "react";
-import { useFetchPlaylistsQuery } from "@/features/playlists/api/playlists-api";
-import { useDebounceValue } from "@/common/hooks";
-import { Pagination } from "@/common/pagination";
-import { PlaylistsList } from "@/features/playlists/ui/playlists-list/playlists-list";
+import {type ChangeEvent, useState} from "react";
+import {useFetchPlaylistsQuery} from "@/features/playlists/api/playlists-api";
+import {useDebounceValue} from "@/common/hooks";
+import {Pagination} from "@/common/pagination";
+import {PlaylistsList} from "@/features/playlists/ui/playlists-list/playlists-list";
 import s from "./playlists-page.module.css";
+import {Skeleton} from "@zheny-zh/common-ui";
 
 export const PlaylistsPage = () => {
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const [pageSize, setPageSize] = useState(2);
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [pageSize, setPageSize] = useState(2);
 
-  const [search, setSearch] = useState<string>("");
+    const [search, setSearch] = useState<string>("");
 
-  const debounceSearch = useDebounceValue(search);
+    const debounceSearch = useDebounceValue(search);
 
-  const { data, isLoading } = useFetchPlaylistsQuery({
-    search: debounceSearch,
-    pageNumber: currentPage,
-    pageSize,
-  });
+    const {data, isLoading} = useFetchPlaylistsQuery({
+        search: debounceSearch,
+        pageNumber: currentPage,
+        pageSize,
+    });
 
-  const changePageSizeHandler = (size: number) => {
-    setPageSize(size);
-    setCurrentPage(1);
-  };
+    const changePageSizeHandler = (size: number) => {
+        setPageSize(size);
+        setCurrentPage(1);
+    };
 
-  const searchPlaylistHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.currentTarget.value);
-    setCurrentPage(1);
-  };
+    const searchPlaylistHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.currentTarget.value);
+        setCurrentPage(1);
+    };
 
-  if (isLoading) return <h1>Loading...</h1>;
+    if (isLoading) return <h1>Loading...</h1>;
 
-  return (
-    <div className={s.container}>
-      <h1>Playlists page</h1>
-      <input
-        type="search"
-        placeholder={"Search playlist by title"}
-        onChange={searchPlaylistHandler}
-      />
-      <PlaylistsList
-        playlists={data?.data ?? []}
-        isPlaylistLoading={isLoading}
-      />
-      <Pagination
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-        pagesCount={data?.meta.pagesCount || 1}
-        pageSize={pageSize}
-        changePageSize={changePageSizeHandler}
-      />
-    </div>
-  );
+    return (
+        <div className={s.container}>
+            <h1>Playlists page</h1>
+            <Skeleton height={12} width={'120px'}/>
+            <input
+                type="search"
+                placeholder={"Search playlist by title"}
+                onChange={searchPlaylistHandler}
+            />
+            <PlaylistsList
+                playlists={data?.data ?? []}
+                isPlaylistLoading={isLoading}
+            />
+            <Pagination
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                pagesCount={data?.meta.pagesCount || 1}
+                pageSize={pageSize}
+                changePageSize={changePageSizeHandler}
+            />
+        </div>
+    );
 };
